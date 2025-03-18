28e4019bb7018459780f30702660197d2000e441
Manish Suthar <manishsuthar078@gmail.com>
2025-03-19 04:36:36 +0530

    build: express-response-handler added which contain FlowError and FlowResponse, In future It may hold error-handler as well

---
+++ packages/express-response-handler/src/FlowError.ts
@@ -0,0 +1,37 @@
+import { Response } from "express";
+// import { isDev } from "../../utils/isDev";
+// import { iFlowError } from "../../../types";
+
+export interface iFlowError {
+  res: Response;
+  status?: number;
+  message?: string;
+  stack?: string | null;
+}
+
+class FlowError {
+  status: number;
+  message: string;
+  stack: string | null;
+  res: Response;
+
+  constructor({ res, status, message, stack }: iFlowError) {
+    if (!res) {
+      throw new Error("Response object is required");
+    }
+    this.res = res;
+    this.status = status || 500;
+    this.message = message || "Internal server error";
+    this.stack = stack || null;
+  }
+
+  send({ status, message, stack }: Partial<iFlowError> = {}) {
+    // if (stack && isDev()) console.log(this.stack);
+
+    return this.res.status(status || +this.status).json({
+      success: status ? status < 400 : +this.status < 400,
+      message: message || this.message,
+    });
+  }
+}
+export default FlowError;
