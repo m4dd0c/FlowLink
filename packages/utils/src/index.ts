102cb6493e8924b784e3690aedc39450f5a5daf4
Manish Suthar <manishsuthar078@gmail.com>
2025-03-27 19:20:02 +0530

    impr: dto func added

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,8 @@
 export const isDev = () => {
   return process.env.NODE_ENV === "development";
 };
+
+export const userDTO = (user: any) => {
+  const { password, ...safeUser } = user;
+  return safeUser;
+};
