f7a04d346aa370f13d39bea5099f0286dbd70237
Manish Suthar <manishsuthar078@gmail.com>
2025-03-27 19:16:43 +0530

    impr: controller added for signup, signin, and getuser

--- apps/backend/src/route/user.ts
+++ apps/backend/src/route/user.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { signUp, signIn, getUser } from "../controller/user";
 import { isAuth } from "../middleware/isAuth";
 
-const router = express.Router();
+const router: express.Router = express.Router();
 
 router.route("/sign-up").post(signUp);
 router.route("/sign-in").post(signIn);
