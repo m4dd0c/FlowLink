3874550142bd20e12fd26292e887133233cf4ac6
Manish Suthar <manishsuthar078@gmail.com>
2025-03-27 19:11:40 +0530

    feat: middleware isAuth added

--- apps/backend/src/middleware/isAuth.ts
+++ apps/backend/src/middleware/isAuth.ts
@@ -1,3 +1,24 @@
+import prisma from "@flowlink/db";
 import catchAsync from "@flowlink/exres/catchAsync";
+import FlowError from "@flowlink/exres/FlowError";
+import { verifySession } from "@flowlink/exres/jwt";
 
-export const isAuth = catchAsync(async (req, res) => {});
+export const isAuth = catchAsync(async (req, res, next) => {
+  const flowError = new FlowError({ res });
+
+  const { token } = req.cookies;
+
+  if (!token)
+    return flowError.send({ status: 400, message: "You are not authorized" });
+
+  const userId = verifySession(token);
+  if (!userId)
+    return flowError.send({ status: 400, message: "You are not authorized" });
+
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+  if (!user)
+    return flowError.send({ status: 400, message: "You are not authorized" });
+
+  req.user = user;
+  next();
+});
