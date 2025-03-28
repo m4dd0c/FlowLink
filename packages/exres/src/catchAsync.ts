0443e2b084e151272f1d3045fac280d46575bc90
Manish Suthar <manishsuthar078@gmail.com>
2025-03-28 19:01:09 +0530

    fix(error): Cannot set headers after they were already sent.

--- packages/exres/src/catchAsync.ts
+++ packages/exres/src/catchAsync.ts
@@ -1,8 +1,7 @@
-import { NextFunction, Response, Request } from "express";
-
+import { Request, Response, NextFunction } from "express";
 const catchAsync =
   (fn: (req: Request, res: Response, next: NextFunction) => any) =>
   (req: Request, res: Response, next: NextFunction) => {
-    new Promise(fn(req, res, next)).catch(next);
+    Promise.resolve(fn(req, res, next)).catch(next);
   };
 export default catchAsync;
