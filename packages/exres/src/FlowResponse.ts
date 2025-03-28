8244f47feac37f0e2115975de2fe0d50f2d3e42b
Manish Suthar <manishsuthar078@gmail.com>
2025-03-28 18:58:50 +0530

    impr: saveSession in send() fn

--- packages/exres/src/FlowResponse.ts
+++ packages/exres/src/FlowResponse.ts
@@ -1,5 +1,7 @@
 import { Response } from "express";
 import { iFlowResponse } from "../types";
+import { isDev } from "@flowlink/utils";
+import { saveSession } from "./jwt";
 
 class FlowResponse {
   status: number;
@@ -14,12 +16,34 @@ class FlowResponse {
     this.message = message || "Done";
     this.data = data || null;
   }
-  send({ status, message, data }: Partial<iFlowResponse> = {}) {
-    return this.res.status(status || this.status).json({
-      success: status ? status < 400 : +this.status < 400,
-      message: message || this.message,
-      data: data || this.data,
-    });
+  send({
+    status,
+    message,
+    data,
+    auth,
+  }: Partial<iFlowResponse> & { auth?: string } = {}) {
+    if (auth) {
+      const token = saveSession(auth);
+      return this.res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: isDev() ? false : true,
+          sameSite: isDev() ? "lax" : "none",
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        })
+        .status(status || this.status)
+        .json({
+          success: status ? status < 400 : +this.status < 400,
+          message: message || this.message,
+          data: data || this.data,
+        });
+    } else {
+      return this.res.status(status || this.status).json({
+        success: status ? status < 400 : +this.status < 400,
+        message: message || this.message,
+        data: data || this.data,
+      });
+    }
   }
 }
 export default FlowResponse;
