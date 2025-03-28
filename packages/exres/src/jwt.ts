009bb1b6d04e1e95f8030bd93f183f65c1c00487
Manish Suthar <manishsuthar078@gmail.com>
2025-03-28 22:54:28 +0530

    chore: console.logs removed

--- packages/exres/src/jwt.ts
+++ packages/exres/src/jwt.ts
@@ -1,11 +1,9 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 export const saveSession = (id: string) => {
   const secret = process.env.JWT_SECRET;
-  console.log("secret", secret);
   if (!secret) throw new Error("JWT_SECRET not found in .env file");
 
   const token = jwt.sign({ id }, secret, { expiresIn: "30d" });
