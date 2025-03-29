7f729ad8bdcac918bf0e7bdf40b1b2672213e466
Manish Suthar <manishsuthar078@gmail.com>
2025-03-29 17:24:15 +0530

    chore: webhook main controller created

--- apps/webhooks/src/app.ts
+++ apps/webhooks/src/app.ts
@@ -1,10 +1,44 @@
+import prisma from "@flowlink/db";
+import catchAsync from "@flowlink/exres/catchAsync";
+import FlowError from "@flowlink/exres/FlowError";
+import FlowResponse from "@flowlink/exres/FlowResponse";
 import express, { Express } from "express";
 
 export const app: Express = express();
 
-app.get("/hooks/catch/:userId/:zapId", (req, res) => {
+const isAuth = catchAsync(async (req, res, next) => {
+  next();
+});
+
+const zapController = catchAsync(async (req, res, next) => {
   const { userId, zapId } = req.params;
+  if (!userId || !zapId)
+    return next(
+      new FlowError({ res, status: 422, message: "Invalid request" }).send(),
+    );
+
+  const metadata = req.body;
+
   console.log(`User ${userId} has Zap ${zapId}`);
 
-  res.sendStatus(200);
+  await prisma.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata,
+      },
+    });
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+  return new FlowResponse({
+    res,
+    status: 200,
+    message: "Webhook received",
+  }).send();
 });
+
+app.post("/hooks/catch/:userId/:zapId", isAuth, zapController);
