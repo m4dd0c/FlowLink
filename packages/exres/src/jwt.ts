06b3a8bd03752d3bb6d234557ec4988c42bdaf08
Manish Suthar <manishsuthar078@gmail.com>
2025-03-29 16:08:16 +0530

    feat: redis added for session management within different backends and workers

--- packages/exres/src/jwt.ts
+++ packages/exres/src/jwt.ts
@@ -1,19 +1,29 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import dotenv from "dotenv";
+import redis from "@flowlink/redis";
+
 dotenv.config();
 
-export const saveSession = (id: string) => {
+export const saveSession = async (id: string) => {
   const secret = process.env.JWT_SECRET;
   if (!secret) throw new Error("JWT_SECRET not found in .env file");
 
   const token = jwt.sign({ id }, secret, { expiresIn: "30d" });
+  try {
+    await redis.set(`session:${id}`, token, "EX", 30 * 24 * 60 * 60);
+  } catch (error) {
+    console.error("Error saving session to redis:", error);
+  }
   return token;
 };
 
-export const verifySession = (token: string) => {
+export const verifySession = async (token: string) => {
   const secret = process.env.JWT_SECRET;
   if (!secret) throw new Error("JWT_SECRET not found in .env file");
   const auth = jwt.verify(token, secret) as JwtPayload & { id: string };
-  if (auth && auth?.id) return auth.id;
-  else return null;
+
+  if (auth && auth?.id) {
+    await redis.get(`session:${auth.id}`);
+  }
+  return auth?.id;
 };
