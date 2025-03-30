5af3162a8e734fba581fd0688bff52c582d802b4
Manish Suthar <manishsuthar078@gmail.com>
2025-03-30 16:22:03 +0530

    chore: redis added

--- packages/exres/src/jwt.ts
+++ packages/exres/src/jwt.ts
@@ -1,29 +1,31 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import dotenv from "dotenv";
-import redis from "@flowlink/redis";
+// import redis from "@flowlink/redis";
 
 dotenv.config();
 
-export const saveSession = async (id: string) => {
+export const saveSession = (id: string) => {
+  console.log("Exres cwd", process.cwd());
   const secret = process.env.JWT_SECRET;
   if (!secret) throw new Error("JWT_SECRET not found in .env file");
 
   const token = jwt.sign({ id }, secret, { expiresIn: "30d" });
-  try {
-    await redis.set(`session:${id}`, token, "EX", 30 * 24 * 60 * 60);
-  } catch (error) {
-    console.error("Error saving session to redis:", error);
-  }
+  // try {
+  //   await redis.set(`session:${id}`, token, "EX", 30 * 24 * 60 * 60);
+  // } catch (error) {
+  //   console.error("Error saving session to redis:", error);
+  // }
   return token;
 };
 
-export const verifySession = async (token: string) => {
+export const verifySession = (token: string) => {
   const secret = process.env.JWT_SECRET;
+  console.log(process.cwd(), "current wd");
   if (!secret) throw new Error("JWT_SECRET not found in .env file");
   const auth = jwt.verify(token, secret) as JwtPayload & { id: string };
 
-  if (auth && auth?.id) {
-    await redis.get(`session:${auth.id}`);
-  }
+  // if (auth && auth?.id) {
+  //   await redis.get(`session:${auth.id}`);
+  // }
   return auth?.id;
 };
