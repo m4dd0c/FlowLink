1777525c53e4e38f685083fa5d2890a0a7e0e49b
Manish Suthar <manishsuthar078@gmail.com>
2025-03-30 16:27:26 +0530

    refactor: path added

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -5,16 +5,19 @@ import { FRONTEND_URL } from ".";
 import cookieParser from "cookie-parser";
 import { setupRoutes } from "./route";
 import error from "@flowlink/exres/error";
+import path from "path";
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 export const app: Express = express();
+console.log("prime cwd", process.cwd(), "seckk,", process.env.JWT_SECRET);
 
 const corsOptions = {
   origin: FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
