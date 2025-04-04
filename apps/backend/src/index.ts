51ea3ad3fdb55da6389d9473ac1fb33c49a23132
Manish Suthar <manishsuthar078@gmail.com>
2025-04-04 18:07:02 +0530

    refactor(env): better naming for dotenv variable.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
 
-const PORT = process.env.PORT || 4000;
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+
+const PORT = process.env.MAIN_SERVER_PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("Primary Server is up & running...");
