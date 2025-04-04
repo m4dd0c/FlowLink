c218405448fb3a3804ee3b534380313f9fcbd8ab
Manish Suthar <manishsuthar078@gmail.com>
2025-04-04 18:09:12 +0530

    refactor(env): better naming for dotenv variable.

--- apps/webhooks/src/index.ts
+++ apps/webhooks/src/index.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.WEBHOOKS_SERVER_PORT || 4001;
 
 app.listen(PORT, () => {
   console.log("Webhooks Server is up & running...");
