deb5e4de315988a0c64f71187963cdcac748e0b5
Manish Suthar <manishsuthar078@gmail.com>
2025-04-06 15:59:56 +0530

    build: processor added, has functionality to take 10 records from outbox table and send them to kafka, once added to kafka they are deleted

--- apps/processor/src/index.ts
+++ apps/processor/src/index.ts
@@ -1,5 +1,41 @@
-function main() {
+import kafka, { KAFKA_TOPIC_NAME } from "@flowlink/kafka";
+import prisma from "@flowlink/db";
+
+async function main() {
+  // Creating a Kafka producer and connecting to the Kafka broker
+  const producer = kafka.producer();
+  await producer.connect();
+
   try {
-  } catch (error) {}
+    // Infinite loop to keep the process alive
+    while (1) {
+      // Step 1: Pull from outbox table
+      const zapRecords = await prisma.zapRunOutbox.findMany({
+        where: {},
+        take: 10,
+      });
+
+      // Step 2: Produce to kafka queue
+      await producer.send({
+        topic: KAFKA_TOPIC_NAME,
+        messages: zapRecords.map((record) => ({
+          value: record.zapRunId,
+        })),
+      });
+
+      // Step 3: Delete from outbox table
+      await prisma.zapRunOutbox.deleteMany({
+        where: {
+          id: {
+            in: zapRecords.map((record) => record.id),
+          },
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error in producer loop:", error);
+  }
 }
+
+// Invoke the main function
 main();
