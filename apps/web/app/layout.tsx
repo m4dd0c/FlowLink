9370beb5eb4b4d6799c7724f39c82f6b333d64dc
Manish Suthar <manishsuthar078@gmail.com>
2025-04-09 00:32:45 +0530

    feat(font): new font playfair introduced

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,13 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
-import { Poppins } from "next/font/google";
+import { Poppins, Playfair } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
+
+const montserrat = Playfair({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
 export const metadata: Metadata = {
   title: { default: "Home", template: "%s | FlowLink" },
   description:
@@ -22,7 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} bg-background text-foreground `}>
+      <body
+        className={`${poppins.variable} ${montserrat.variable} bg-background text-foreground `}
+      >
         <Header />
         <main>{children}</main>
         <Footer />
