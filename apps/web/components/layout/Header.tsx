bc08addbf0171c27e7d394a25c3bc3e6a538b129
Manish Suthar <manishsuthar078@gmail.com>
2025-04-09 00:33:28 +0530

    chore: tried both fonts

--- apps/web/components/layout/Header.tsx
+++ apps/web/components/layout/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="">
+      <h1 className="bg-red-500/50 font-playfair">Default 2</h1>
+      <h1 className="bg-red-500/50 font-poppins">Default 2</h1>
+    </div>
+  );
 };
 
 export default Header;
