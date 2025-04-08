5c9d197f6d39bbee5b7366f7c256b4ec03a756aa
Manish Suthar <manishsuthar078@gmail.com>
2025-04-08 23:37:10 +0530

    chore: bg-background and bg-foreground used

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>
+      <body className={`${poppins.className} bg-background text-foreground `}>
         <Header />
         <main>{children}</main>
         <Footer />
