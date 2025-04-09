fc0545fce743f7c18ce47086163e427401189aad
Manish Suthar <manishsuthar078@gmail.com>
2025-04-09 21:22:49 +0530

    feat(forms): add SignupForm component
    Introduce a SignupForm component with name email and password fields,
    validation using react-hook-form and zod, and links for log-in. Includes a submit button and placeholder
    text for user guidance.
    
    This component is designed to streamline user authentication
    processes and improve the sign-up experience.

---
+++ apps/web/components/forms/Signup.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { SignupFormSchema } from "@/lib/schema/schema";
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof SignupFormSchema>>({
+    resolver: zodResolver(SignupFormSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SignupFormSchema>) => {
+    console.log("submitted", values);
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl">Log in to your account.</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="e.g., John Doe" {...field} />
+                  <FormMessage />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="e.g., user@address.tld"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="e.g., s7r0ngP@ssw0rd"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  <Link
+                    href="/forgot-password"
+                    className="text-sm text-blue-500 hover:underline"
+                  >
+                    Forgot password?
+                  </Link>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <small>
+            Don&apos;t have an account? <Link href="/sign-up">Sign up</Link>
+          </small>
+          <Button type="submit">Log in</Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
