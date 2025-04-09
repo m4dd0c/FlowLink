6763a89b98766709305500e37a22bd4587ba3dc2
Manish Suthar <manishsuthar078@gmail.com>
2025-04-09 22:06:31 +0530

    feat: changes made according to shadcn theme mode colors

--- apps/web/components/home/Hero.tsx
+++ apps/web/components/home/Hero.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
@@ -17,16 +18,16 @@ const Hero = () => {
             your imagination.
           </p>
           <div className="flex gap-4 mt-4">
+            <Button variant="outline" className="rounded-full">
+              Learn More
+            </Button>
             <Link
               href="/sign-up"
-              className="flex items-center justify-center gap-2 bg-foreground/10 rounded-full px-5 py-1 text-white"
+              className="flex items-center justify-center gap-2 bg-accent rounded-full px-5 py-1"
             >
               <p>✨ Get Started</p>
               <FaArrowRightLong className="animate-bounce-x" />
             </Link>
-            <button className="border-foreground/20 border-1 rounded-full px-5 py-1">
-              Learn More
-            </button>
           </div>
         </div>
         <div className="flex-1">
@@ -35,7 +36,7 @@ const Hero = () => {
             alt="homepage-hero"
             height={500}
             width={500}
-            className="h-auto w-5/6 object-contain invert-100 float-right"
+            className="h-auto w-5/6 object-contain float-right rounded-xl"
             priority
           />
         </div>
