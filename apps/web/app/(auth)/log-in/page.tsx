4db8fcd92039036e04f11d7713344684c493caab
Manish Suthar <manishsuthar078@gmail.com>
2025-04-09 22:57:32 +0530

    chore: copied text changed, and fixed

--- apps/web/app/(auth)/log-in/page.tsx
+++ apps/web/app/(auth)/log-in/page.tsx
@@ -1,18 +1,20 @@
 import LoginForm from "@/components/forms/Login";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Login = () => {
   return (
-    <div className="h-screen container mx-auto border">
-      <div className="flex-1">
+    <div className="flex mt-24 max-md:flex-col h-screen container mx-auto space-y-8">
+      <div className="flex-1 space-y-4">
         <h1 className="text-4xl font-bold">Automate across your teams</h1>
-        <p>
-          Zapier Enterprise empowers everyone in your business to securely
-          automate their work in minutes, not months—no coding required.
+        <p className="text-xl">
+          FlowLink empowers everyone in your business to securely automate their
+          work in minutes, not months—no coding required.
         </p>
-        <button className="border rounded-full px-4 py-1">
-          Explore Zapier Enterprise
-        </button>
+        <Button>
+          <Link href="/">Explore FlowLink</Link>
+        </Button>
       </div>
       <div className="flex-1">
         <LoginForm />
--- apps/web/app/(auth)/sign-up/page.tsx
+++ apps/web/app/(auth)/sign-up/page.tsx
@@ -1,24 +1,24 @@
-import SignupForm from "@/components/forms/Login";
+import SignupForm from "@/components/forms/Signup";
 import React from "react";
 import { IoCheckmarkCircle } from "react-icons/io5";
 
 const Login = () => {
   return (
-    <div className="h-screen container mx-auto border">
-      <div className="flex-1">
+    <div className="flex mt-24 max-md:flex-col h-screen container mx-auto space-y-8">
+      <div className="flex-1 space-y-4">
         <h1 className="text-4xl font-bold">
-          Join millions worldwide who automate their work using Zapier.
+          Join millions worldwide who automate their work using FlowLink.
         </h1>
-        <ul>
-          <li>
+        <ul className="space-y-2">
+          <li className="flex gap-1 items-center">
             <IoCheckmarkCircle />
             <p>Easy setup, no coding required</p>
           </li>
-          <li>
+          <li className="flex gap-1 items-center">
             <IoCheckmarkCircle />
             <p>Free forever for core features</p>
           </li>
-          <li>
+          <li className="flex gap-1 items-center">
             <IoCheckmarkCircle />
             <p>14-day trial of premium features & apps</p>
           </li>
