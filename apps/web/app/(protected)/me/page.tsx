866861249afdb09e649d27c737f6b930ca338f1e
Manish Suthar <manishsuthar078@gmail.com>
2025-04-10 23:24:33 +0530

    chore: dummy profile page added

--- apps/web/app/(protected)/me/page.tsx
+++ apps/web/app/(protected)/me/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useMeQuery } from "@/store/api/user";
 
 const Page = () => {
-  return <div>this is profile page</div>;
+  const { data, isFetching, isError } = useMeQuery(null);
+
+  useEffect(() => {
+    if (isError) {
+      console.error("Error fetching user data");
+    }
+  }, [isError]);
+
+  if (isFetching) return <div>Loading...</div>;
+  return <div>this is profile page {JSON.stringify(data?.data)}</div>;
 };
 
 export default Page;
