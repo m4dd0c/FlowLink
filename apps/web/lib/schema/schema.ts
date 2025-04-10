192f5a70558352aa324efba4e7ebe1da15fd00bb
Manish Suthar <manishsuthar078@gmail.com>
2025-04-10 17:47:35 +0530

    fix: removed wrong email regex and used zod-inbuilt function

--- apps/web/lib/schema/schema.ts
+++ apps/web/lib/schema/schema.ts
@@ -1,18 +1,13 @@
 import { z } from "zod";
 
 const SignupFormSchema = z.object({
-  email: z
-    .string()
-    .min(1, "Email is required")
-    .regex(/^[A-z0-9]+@+[A-z0-9].[A-z0-9]{0,4}$/, "Invalid email address"),
+  email: z.string().min(1, "Email is required").email("Invalid Email address"),
+
   name: z.string().min(3, "Name must have at least 3 characters"),
   password: z.string().min(8, "Password must have at least 8 characters"),
 });
 const LoginFormSchema = z.object({
-  email: z
-    .string()
-    .min(1, "Email is required")
-    .regex(/^[A-z0-9]+@+[A-z0-9].[A-z0-9]{0,4}$/, "Invalid email address"),
+  email: z.string().min(1, "Email is required").email("Invalid Email address"),
   password: z.string().min(8, "Password must have at least 8 characters"),
 });
 
