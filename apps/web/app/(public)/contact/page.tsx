d7f6c4708e7de44592ae3176902c8a144902ccca
Manish Suthar <manishsuthar078@gmail.com>
2025-04-10 23:29:33 +0530

    chore: data changed to contact page specific data

--- apps/web/app/(public)/contact/page.tsx
+++ apps/web/app/(public)/contact/page.tsx
@@ -6,21 +6,19 @@ const Contact = () => {
   return (
     <div className="flex mt-24 max-md:flex-col h-screen container mx-auto space-y-8">
       <div className="flex-1 space-y-4">
-        <h1 className="text-4xl font-bold">
-          Join millions worldwide who automate their work using FlowLink.
-        </h1>
+        <h1 className="text-4xl font-bold">Get in Touch with Us</h1>
         <ul className="space-y-2">
           <li className="flex gap-1 items-center">
             <IoCheckmarkCircle />
-            <p>Easy setup, no coding required</p>
+            <p>24/7 Customer Support</p>
           </li>
           <li className="flex gap-1 items-center">
             <IoCheckmarkCircle />
-            <p>Free forever for core features</p>
+            <p>Reach us via email or phone</p>
           </li>
           <li className="flex gap-1 items-center">
             <IoCheckmarkCircle />
-            <p>14-day trial of premium features & apps</p>
+            <p>Dedicated support for premium users</p>
           </li>
         </ul>
       </div>
