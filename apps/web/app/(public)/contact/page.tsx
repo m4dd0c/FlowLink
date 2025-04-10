2a9a1d2d6d39bbeacf676c4608eccd773355e61f
Manish Suthar <manishsuthar078@gmail.com>
2025-04-10 23:24:09 +0530

    build: dummy contact page added

--- apps/web/app/(public)/contact/page.tsx
+++ apps/web/app/(public)/contact/page.tsx
@@ -1,7 +1,34 @@
+import ContactForm from "@/components/forms/Contact";
 import React from "react";
+import { IoCheckmarkCircle } from "react-icons/io5";
 
-const Page = () => {
-  return <div>CONTACT PAGE</div>;
+const Contact = () => {
+  return (
+    <div className="flex mt-24 max-md:flex-col h-screen container mx-auto space-y-8">
+      <div className="flex-1 space-y-4">
+        <h1 className="text-4xl font-bold">
+          Join millions worldwide who automate their work using FlowLink.
+        </h1>
+        <ul className="space-y-2">
+          <li className="flex gap-1 items-center">
+            <IoCheckmarkCircle />
+            <p>Easy setup, no coding required</p>
+          </li>
+          <li className="flex gap-1 items-center">
+            <IoCheckmarkCircle />
+            <p>Free forever for core features</p>
+          </li>
+          <li className="flex gap-1 items-center">
+            <IoCheckmarkCircle />
+            <p>14-day trial of premium features & apps</p>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1">
+        <ContactForm />
+      </div>
+    </div>
+  );
 };
 
-export default Page;
+export default Contact;
