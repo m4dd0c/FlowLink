f09aa410983bc459f83f271f522c1be0952821bd
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 00:00:12 +0530

    impr(ui): page has uniform horizontal spacing now

--- apps/web/app/(public)/contact/page.tsx
+++ apps/web/app/(public)/contact/page.tsx
@@ -5,7 +5,7 @@ import { IoCheckmarkCircle } from "react-icons/io5";
 const Contact = () => {
   return (
     <div className="grid grid-cols-2 place-items-center max-md:grid-cols-1 mt-24 container mx-auto space-y-8">
-      <div className="space-y-4">
+      <div className="w-5/6 space-y-4">
         <h1 className="text-4xl font-bold">Get in Touch with Us</h1>
         <ul className="space-y-2">
           <li className="flex gap-1 items-center">
