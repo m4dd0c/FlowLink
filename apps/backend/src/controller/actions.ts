1f79077d291b943d6a29aa30c57b46b5026d867b
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 01:56:55 +0530

    build: endpoint /available/actions controller logic added

--- apps/backend/src/controller/actions.ts
+++ apps/backend/src/controller/actions.ts
@@ -1,3 +1,18 @@
 import catchAsync from "@flowlink/exres/catchAsync";
+import FlowError from "@flowlink/exres/FlowError";
+import prisma from "@flowlink/db";
+import FlowResponse from "@flowlink/exres/FlowResponse";
 
-export const availableActions = catchAsync(async (req, res) => {});
+export const availableActions = catchAsync(async (req, res) => {
+  const flowError = new FlowError({ res });
+  const user = (req as any).user;
+
+  if (!user)
+    return flowError.send({
+      status: 401,
+      message: "Unauthorized access.",
+    });
+
+  const availableActions = await prisma.availableActions.findMany({});
+  return new FlowResponse({ res, data: availableActions }).send();
+});
