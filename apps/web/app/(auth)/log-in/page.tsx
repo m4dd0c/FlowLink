64533dc519e2c7f31a3215e086a72db598a517ad
Manish Suthar <manishsuthar078@gmail.com>
2025-04-11 23:59:20 +0530

    impr(ui): page has uniform horizontal spacing now

--- apps/web/app/(auth)/log-in/page.tsx
+++ apps/web/app/(auth)/log-in/page.tsx
@@ -7,7 +7,7 @@ import { FaRegCompass } from "react-icons/fa6";
 const Login = () => {
   return (
     <div className="grid grid-cols-2 place-items-center max-md:grid-cols-1 mt-24 container mx-auto space-y-8">
-      <div className="space-y-4">
+      <div className="w-5/6 space-y-4">
         <h1 className="text-4xl font-bold">Automate across your teams</h1>
         <p className="text-xl">
           FlowLink empowers everyone in your business to securely automate their
