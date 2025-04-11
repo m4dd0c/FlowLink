6c91cd173e90c6510f1e815e0e1571027b3da822
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 01:17:58 +0530

    impr(schema): email schema improved by using inbuilt zod function instead of relying on regex

--- apps/backend/src/lib/zodSchemas/user.ts
+++ apps/backend/src/lib/zodSchemas/user.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 export const SignUpSchema = z.object({
   name: z.string().min(3).max(40),
-  email: z.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+  email: z.string().min(1, "Email is required").email("Invalid email."),
   password: z.string().min(6).max(40),
 });
 
 export const SignInSchema = z.object({
-  email: z.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+  email: z.string().min(1, "Email is required").email("Invalid email."),
   password: z.string().min(6).max(40),
 });
