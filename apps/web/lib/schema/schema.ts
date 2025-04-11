21b48faa61f6f70dd53ddd5a936568709b636c0d
Manish Suthar <manishsuthar078@gmail.com>
2025-04-11 23:30:46 +0530

    build: zod schema added for contactform

--- apps/web/lib/schema/schema.ts
+++ apps/web/lib/schema/schema.ts
@@ -2,13 +2,22 @@ import { z } from "zod";
 
 const SignupFormSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid Email address"),
-
   name: z.string().min(3, "Name must have at least 3 characters"),
   password: z.string().min(8, "Password must have at least 8 characters"),
 });
+
 const LoginFormSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid Email address"),
   password: z.string().min(8, "Password must have at least 8 characters"),
 });
 
-export { LoginFormSchema, SignupFormSchema };
+const ContactFormSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid Email address"),
+  name: z.string().min(3, "Name must have at least 3 characters"),
+  message: z
+    .string()
+    .min(1, "Message is required.")
+    .max(1000, "Message length can not exceed 1,000 characters"),
+});
+
+export { LoginFormSchema, SignupFormSchema, ContactFormSchema };
