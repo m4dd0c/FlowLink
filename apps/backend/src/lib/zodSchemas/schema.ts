e61115539ab7057c78c06d7ab1ed543fe8d17e37
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 02:12:02 +0530

    feat: zapIdSchema created

--- apps/backend/src/lib/zodSchemas/schema.ts
+++ apps/backend/src/lib/zodSchemas/schema.ts
@@ -1,12 +1,18 @@
 import { z } from "zod";
 
-export const SignUpSchema = z.object({
+const SignUpSchema = z.object({
   name: z.string().min(3).max(40),
   email: z.string().min(1, "Email is required").email("Invalid email."),
   password: z.string().min(6).max(40),
 });
 
-export const SignInSchema = z.object({
+const SignInSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email."),
   password: z.string().min(6).max(40),
 });
+
+const ZapIdSchema = z.object({
+  zapId: z.string().min(1, "ZapId is required").cuid("Invalid zap id."),
+});
+
+export { SignUpSchema, SignInSchema, ZapIdSchema };
