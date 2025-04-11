8129fc9279c1ac3fd43ec90ae7c6aa5cb1ee2d0e
Manish Suthar <manishsuthar078@gmail.com>
2025-04-11 23:35:13 +0530

    build: contact page created

--- apps/web/components/forms/Contact.tsx
+++ apps/web/components/forms/Contact.tsx
@@ -1,7 +1,99 @@
+"use client";
+
 import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import { ContactFormSchema } from "@/lib/schema/schema";
 
 const ContactForm = () => {
-  return <div>Contact</div>;
+  const form = useForm<z.infer<typeof ContactFormSchema>>({
+    resolver: zodResolver(ContactFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof ContactFormSchema>) => {
+    try {
+      console.log("submitted");
+    } catch (err: any) {
+      console.error(err?.response?.data.message);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl mb-8">Know us.</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="e.g., John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="e.g., user@address.tld"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="e.g., FlowLink is awesome..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <br />
+          <Button className="my-1" type="submit">
+            Send
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
 };
 
 export default ContactForm;
