44a11d522bf05f8cb47e2f75bbaac1ffbbdf4ccf
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 01:57:12 +0530

    build: endpoint /available/triggers controller logic added

--- apps/backend/src/controller/triggers.ts
+++ apps/backend/src/controller/triggers.ts
@@ -1,3 +1,18 @@
 import catchAsync from "@flowlink/exres/catchAsync";
+import FlowError from "@flowlink/exres/FlowError";
+import prisma from "@flowlink/db";
+import FlowResponse from "@flowlink/exres/FlowResponse";
 
-export const availableTriggers = catchAsync(async (req, res) => {});
+export const availableTriggers = catchAsync(async (req, res) => {
+  const flowError = new FlowError({ res });
+  const user = (req as any).user;
+
+  if (!user)
+    return flowError.send({
+      status: 401,
+      message: "Unauthorized access.",
+    });
+
+  const availableTriggers = await prisma.availableTriggers.findMany({});
+  return new FlowResponse({ res, data: availableTriggers }).send();
+});
