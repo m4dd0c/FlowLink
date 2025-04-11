0d7119848ca85e080d798ea1d70c5fbe15203b5e
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 02:12:20 +0530

    build: getSingleZap controller created

--- apps/backend/src/controller/zaps.ts
+++ apps/backend/src/controller/zaps.ts
@@ -2,6 +2,7 @@ import prisma from "@flowlink/db";
 import catchAsync from "@flowlink/exres/catchAsync";
 import FlowError from "@flowlink/exres/FlowError";
 import FlowResponse from "@flowlink/exres/FlowResponse";
+import { ZapIdSchema } from "../lib/zodSchemas/schema";
 
 export const createZap = catchAsync(async (req, res) => {});
 
@@ -30,8 +31,39 @@ export const getZaps = catchAsync(async (req, res) => {
 });
 
 export const getSingleZap = catchAsync(async (req, res) => {
+  const flowError = new FlowError({ res });
+
   const { zapId } = req.params;
+
+  const validation = ZapIdSchema.safeParse(zapId);
+  if (!validation.success)
+    return flowError.send({
+      status: 422,
+      message: "Invalid input",
+    });
+
+  const user = (req as any).user;
+  if (!user)
+    return flowError.send({
+      status: 401,
+      message: "Unauthorized access.",
+    });
+
+  const zap = await prisma.zap.findFirst({
+    where: { id: zapId, userId: user.id },
+    include: {
+      trigger: {
+        select: { type: true },
+      },
+      actions: {
+        select: { type: true },
+      },
+    },
+  });
+
+  return new FlowResponse({ res, data: zap }).send();
 });
+
 export const deleteZap = catchAsync(async (req, res) => {
   const { zapId } = req.params;
 });
