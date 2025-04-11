2d8e900005cf0d451bbc718f2e358513c34b5485
Manish Suthar <manishsuthar078@gmail.com>
2025-04-12 01:58:05 +0530

    build: endpoint /zaps/ controller logic added for /GET

--- apps/backend/src/controller/zaps.ts
+++ apps/backend/src/controller/zaps.ts
@@ -1,7 +1,34 @@
+import prisma from "@flowlink/db";
 import catchAsync from "@flowlink/exres/catchAsync";
+import FlowError from "@flowlink/exres/FlowError";
+import FlowResponse from "@flowlink/exres/FlowResponse";
 
-export const getZaps = catchAsync(async (req, res) => {});
 export const createZap = catchAsync(async (req, res) => {});
+
+export const getZaps = catchAsync(async (req, res) => {
+  const flowError = new FlowError({ res });
+  const user = (req as any).user;
+  if (!user)
+    return flowError.send({
+      status: 401,
+      message: "Unauthorized access.",
+    });
+
+  const zaps = await prisma.zap.findMany({
+    where: { userId: user.id },
+    include: {
+      trigger: {
+        select: { type: true },
+      },
+      actions: {
+        select: { type: true },
+      },
+    },
+  });
+
+  return new FlowResponse({ res, data: zaps }).send();
+});
+
 export const getSingleZap = catchAsync(async (req, res) => {
   const { zapId } = req.params;
 });
