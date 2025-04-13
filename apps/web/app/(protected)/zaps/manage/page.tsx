1b2420d415dafe6d6a39e589c9c6b948323e7daa
Manish Suthar <manishsuthar078@gmail.com>
2025-04-13 23:45:58 +0530

    feat: getAllZapsQuery used in zaps/manage page

--- apps/web/app/(protected)/zaps/manage/page.tsx
+++ apps/web/app/(protected)/zaps/manage/page.tsx
@@ -13,11 +13,18 @@ import { HiOutlineTrash } from "react-icons/hi2";
 import { FaRegEye } from "react-icons/fa6";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useGetAllZapsQuery } from "@/store/api/zaps";
+import { tUnknownObj } from "@/types";
 
 const ManageZap = () => {
-  const handleZapDelete = (id: string) => {
+  const { isFetching, data } = useGetAllZapsQuery(null);
+
+  console.log(data);
+  const handleZapDelete = (zapId?: string) => {
+    if (!zapId) return;
     alert("delete");
   };
+  if (isFetching) return <h1>Loading...</h1>;
   return (
     <Table>
       <TableCaption>A list of your zaps.</TableCaption>
@@ -31,33 +38,35 @@ const ManageZap = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">1</TableCell>
-          <TableCell>Zap to Send Email on:event Github Comment</TableCell>
-          <TableCell>Github</TableCell>
-          <TableCell>2</TableCell>
-          <TableCell className="flex justify-end items-center gap-2">
-            <Button
-              variant="outline"
-              className="flex items-center justify-center gap-2"
-              asChild
-            >
-              <Link href="/zaps/:zap">
-                <p>View</p>
-                <FaRegEye className="" />
-              </Link>
-            </Button>
+        {data?.data.map((zap: tUnknownObj, idx: number) => (
+          <TableRow key={zap?.id}>
+            <TableCell className="font-medium">{idx + 1}</TableCell>
+            <TableCell>{zap?.title}</TableCell>
+            <TableCell>{zap?.trigger?.type?.name}</TableCell>
+            <TableCell>{zap?.actions?.length}</TableCell>
+            <TableCell className="flex justify-end items-center gap-2">
+              <Button
+                variant="outline"
+                className="flex items-center justify-center gap-2"
+                asChild
+              >
+                <Link href={`/zaps/${zap?.id}`}>
+                  <p>View</p>
+                  <FaRegEye className="" />
+                </Link>
+              </Button>
 
-            <Button
-              onClick={() => handleZapDelete("id")}
-              variant="destructive"
-              className="flex items-center justify-center gap-2 cursor-pointer"
-            >
-              <p>Delete</p>
-              <HiOutlineTrash className="" />
-            </Button>
-          </TableCell>
-        </TableRow>
+              <Button
+                onClick={() => handleZapDelete(zap?.id)}
+                variant="destructive"
+                className="flex items-center justify-center gap-2 cursor-pointer"
+              >
+                <p>Delete</p>
+                <HiOutlineTrash className="" />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
