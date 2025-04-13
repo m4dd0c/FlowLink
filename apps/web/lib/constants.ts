dc75112dab96dd584eea750e7da7fc6249df4b7f
Manish Suthar <manishsuthar078@gmail.com>
2025-04-13 22:27:55 +0530

    build: ...constants added

---
+++ apps/web/lib/constants.ts
@@ -0,0 +1,7 @@
+export const PRIMARY_BE_URL = "http://localhost:4000";
+export const WEBHOOK_BE_URL = "http://localhost:4001";
+
+export const tagTypes = {
+  user: "USER",
+  zaps: "ZAPS",
+} as const;
