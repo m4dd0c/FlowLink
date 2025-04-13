eeab570c7c8f8ceff632a9b2cbbf644622fa7e37
Manish Suthar <manishsuthar078@gmail.com>
2025-04-13 22:30:03 +0530

    feat(zap): zaps reducer added

--- apps/web/store/index.ts
+++ apps/web/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import user from "./api/user";
+import zaps from "./api/zaps";
 
 const store = configureStore({
   reducer: {
     [user.reducerPath]: user.reducer,
+    [zaps.reducerPath]: zaps.reducer,
   },
-  middleware: (gDM) => gDM().concat(user.middleware),
+  middleware: (gDM) => gDM().concat(user.middleware, zaps.middleware),
 });
 setupListeners(store.dispatch);
 
