138f7edfb82c4ee54c46c666109ba8f20cc15d37
Manish Suthar <manishsuthar078@gmail.com>
2025-04-13 22:47:50 +0530

    build: zap specific schemas added

--- apps/web/lib/schema/schema.ts
+++ apps/web/lib/schema/schema.ts
@@ -1,16 +1,27 @@
 import { z } from "zod";
 
+// User specific schemas
 const SignupFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Name must have at least 3 characters")
+    .max(40, "Name must have at most 40 characters."),
   email: z.string().min(1, "Email is required").email("Invalid Email address"),
-  name: z.string().min(3, "Name must have at least 3 characters"),
-  password: z.string().min(8, "Password must have at least 8 characters"),
+  password: z
+    .string()
+    .min(8, "Password must have at least 8 characters.")
+    .max(40, "Password must have at most 40 characters."),
 });
 
 const LoginFormSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid Email address"),
-  password: z.string().min(8, "Password must have at least 8 characters"),
+  password: z
+    .string()
+    .min(8, "Password must have at least 8 characters.")
+    .max(40, "Password must have at most 40 characters."),
 });
 
+// Other schemas
 const ContactFormSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid Email address"),
   name: z.string().min(3, "Name must have at least 3 characters"),
@@ -20,4 +31,30 @@ const ContactFormSchema = z.object({
     .max(1000, "Message length can not exceed 1,000 characters"),
 });
 
-export { LoginFormSchema, SignupFormSchema, ContactFormSchema };
+// Zaps specific schemas
+const ZapIdSchema = z.object({
+  zapId: z.string().min(1, "ZapId is required").cuid("Invalid zap id."),
+});
+
+const ZapCreateSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must contain at least 3 characters.")
+    .max(40, "Title must contain at most 40 characters."),
+  availableTriggerId: z.string(),
+  triggerMetadata: z.any().optional(),
+  actions: z.array(
+    z.object({
+      availableActionId: z.string(),
+      actionMetadata: z.any().optional(),
+    }),
+  ),
+});
+
+export {
+  LoginFormSchema,
+  SignupFormSchema,
+  ContactFormSchema,
+  ZapIdSchema,
+  ZapCreateSchema,
+};
