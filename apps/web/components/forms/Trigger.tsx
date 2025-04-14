66d495bf40c3d31f471d323ac7b9947d5cff843f
Manish Suthar <manishsuthar078@gmail.com>
2025-04-14 20:13:31 +0530

    feat: triggerForm halfway implemented; pending: add default values from node prop

--- apps/web/components/forms/Trigger.tsx
+++ apps/web/components/forms/Trigger.tsx
@@ -22,12 +22,17 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Button } from "../ui/button";
 import { TriggerNodeSchema } from "@/lib/schema/schema";
-import { useRouter } from "next/navigation";
+import { useGetAvailableTriggersQuery } from "@/store/api/ancillary";
 
-const TriggerForm = ({ node }: { node: any }) => {
-  // const {data} = useTriggerNodeSchema.parseAsync(node);
-  // const [trigger] = useLoginMutation();
-  const route = useRouter();
+const TriggerForm = ({
+  node,
+  setIsDrawerOpen,
+}: {
+  node: any;
+  setIsDrawerOpen: (open: boolean) => void;
+}) => {
+  const { isFetching, data: availableTriggers } =
+    useGetAvailableTriggersQuery("");
 
   const form = useForm<z.infer<typeof TriggerNodeSchema>>({
     resolver: zodResolver(TriggerNodeSchema),
@@ -40,87 +45,87 @@ const TriggerForm = ({ node }: { node: any }) => {
 
   const onSubmit = async (values: z.infer<typeof TriggerNodeSchema>) => {
     try {
-      // const res = await trigger(values).unwrap();
-      console.log("done");
-      route.push("/me");
+      if (!values.availableTriggerId)
+        return form.setError("availableTriggerId", {
+          type: "custom",
+          message: "Please select a trigger",
+        });
+      console.log("done", values);
+      setIsDrawerOpen(false);
     } catch (err: any) {
       console.error(err?.response?.data.message);
     }
   };
 
+  if (isFetching) return <h1>Loading...</h1>;
   return (
-    <>
-      <h1 className="text-2xl mb-8">Log in to your account.</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Title</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="e.g., event: Github Comment"
-                    {...field}
-                  />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="e.g., event: Github Comment"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="availableTriggerId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Trigger</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl className="w-full">
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a Trigger/App" />
+                  </SelectTrigger>
                 </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="availableTriggerId"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Trigger</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a Trigger/App" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    {availableTriggers.map((trigger) => (
-                      <SelectItem key={trigger.id} value={trigger.id}>
-                        {trigger.trigger.title}
+                <SelectContent>
+                  {availableTriggers?.data?.map(
+                    (trigger: { id: string; name: string }) => (
+                      <SelectItem key={trigger?.id} value={trigger?.id}>
+                        {trigger?.name}
                       </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="triggerMetadata"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Trigger Metadata</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="e.g., s7r0ngP@ssw0rd"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <br />
-          <Button className="my-1" type="submit">
-            Log in
-          </Button>
-        </form>
-      </Form>
-    </>
+                    ),
+                  )}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="triggerMetadata"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Trigger Metadata</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="e.g., s7r0ngP@ssw0rd"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Done
+        </Button>
+      </form>
+    </Form>
   );
 };
 
