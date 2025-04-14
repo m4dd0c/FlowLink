cc58101661db613191df62f83cd31846961c2d4b
Manish Suthar <manishsuthar078@gmail.com>
2025-04-14 19:38:13 +0530

    feat: ancillary api embedded

--- apps/web/store/index.ts
+++ apps/web/store/index.ts
@@ -2,13 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import user from "./api/user";
 import zaps from "./api/zaps";
+import ancillary from "./api/ancillary";
 
 const store = configureStore({
   reducer: {
     [user.reducerPath]: user.reducer,
     [zaps.reducerPath]: zaps.reducer,
+    [ancillary.reducerPath]: ancillary.reducer,
   },
-  middleware: (gDM) => gDM().concat(user.middleware, zaps.middleware),
+  middleware: (gDM) =>
+    gDM().concat(user.middleware, zaps.middleware, ancillary.middleware),
 });
 setupListeners(store.dispatch);
 
