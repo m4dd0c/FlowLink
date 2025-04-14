cf2acdbac2b4b534275e88fcd84d856797801bbc
Manish Suthar <manishsuthar078@gmail.com>
2025-04-14 20:12:50 +0530

    feat: actionForm halfway implemented

--- apps/web/components/forms/Action.tsx
+++ apps/web/components/forms/Action.tsx
@@ -4,105 +4,129 @@ import React from "react";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "../ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useForm } from "react-hook-form";
 import { Input } from "../ui/Input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Button } from "../ui/button";
-import Link from "next/link";
-import { LoginFormSchema } from "@/lib/schema/schema";
-import { useLoginMutation } from "@/store/api/user";
-import { useRouter } from "next/navigation";
+import { ActionNodeSchema } from "@/lib/schema/schema";
+import { useGetAvailableActionsQuery } from "@/store/api/ancillary";
 
-const ActionForm = ({ node }: { node: any }) => {
-  const [trigger] = useLoginMutation();
-  const route = useRouter();
+const TriggerForm = ({
+  node,
+  setIsDrawerOpen,
+}: {
+  node: any;
+  setIsDrawerOpen: (open: boolean) => void;
+}) => {
+  const { isFetching, data: availableActions } =
+    useGetAvailableActionsQuery("");
 
-  const form = useForm<z.infer<typeof LoginFormSchema>>({
-    resolver: zodResolver(LoginFormSchema),
+  const form = useForm<z.infer<typeof ActionNodeSchema>>({
+    resolver: zodResolver(ActionNodeSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      title: "",
+      availableActionId: "",
+      actionMetadata: "",
     },
   });
 
-  const onSubmit = async (values: z.infer<typeof LoginFormSchema>) => {
+  const onSubmit = async (values: z.infer<typeof ActionNodeSchema>) => {
     try {
-      const res = await trigger(values).unwrap();
-      console.log(res);
-      route.push("/me");
+      if (!values.availableActionId)
+        return form.setError("availableActionId", {
+          type: "custom",
+          message: "Please select an Action",
+        });
+      console.log("done", values);
+      setIsDrawerOpen(false);
     } catch (err: any) {
       console.error(err?.response?.data.message);
     }
   };
 
+  if (isFetching) return <h1>Loading...</h1>;
   return (
-    <>
-      <h1 className="text-2xl mb-8">Log in to your account.</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="e.g., user@address.tld"
-                    {...field}
-                  />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="e.g., event: Send Solana"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="availableActionId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Action type</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl className="w-full">
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select an Action" />
+                  </SelectTrigger>
                 </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="e.g., s7r0ngP@ssw0rd"
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription>
-                  <Link
-                    href="/forgot-password"
-                    className="text-sm text-orange-500 hover:underline"
-                  >
-                    Forgot password?
-                  </Link>
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <small>
-            Don&apos;t have an account?{" "}
-            <Link href="/sign-up" className="text-orange-500">
-              Sign up
-            </Link>
-          </small>
-          <br />
-          <Button className="my-1" type="submit">
-            Log in
-          </Button>
-        </form>
-      </Form>
-    </>
+                <SelectContent>
+                  {availableActions?.data?.map(
+                    (action: { id: string; name: string }) => (
+                      <SelectItem key={action?.id} value={action?.id}>
+                        {action?.name}
+                      </SelectItem>
+                    ),
+                  )}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="actionMetadata"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Action Metadata</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="e.g., s7r0ngP@ssw0rd"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Done
+        </Button>
+      </form>
+    </Form>
   );
 };
 
-export default ActionForm;
+export default TriggerForm;
