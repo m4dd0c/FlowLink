4d4d69f54eccb6c57e3121ee3decf0076ebeb48e
Manish Suthar <manishsuthar078@gmail.com>
2025-04-15 00:44:10 +0530

    feat: ancillary  slice added for storing triggers and actions

--- apps/web/store/index.ts
+++ apps/web/store/index.ts
@@ -3,12 +3,14 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import user from "./api/user";
 import zaps from "./api/zaps";
 import ancillary from "./api/ancillary";
+import ancillarySlice from "./slices/ancillary";
 
 const store = configureStore({
   reducer: {
     [user.reducerPath]: user.reducer,
     [zaps.reducerPath]: zaps.reducer,
     [ancillary.reducerPath]: ancillary.reducer,
+    ancillarySlice: ancillarySlice,
   },
   middleware: (gDM) =>
     gDM().concat(user.middleware, zaps.middleware, ancillary.middleware),
