c9b375785cf5ec0e562bef88425a3c8cae3cc4a7
Manish Suthar <manishsuthar078@gmail.com>
2025-04-15 14:23:09 +0530

    refactor: label and title interchanged (you can ignore that; its just better naming convention
    Label used as Trigger or Action name

--- apps/web/app/(protected)/zaps/publish/Node.tsx
+++ apps/web/app/(protected)/zaps/publish/Node.tsx
@@ -1,10 +1,15 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { iAncillarySliceState } from "@/types";
+import { iAncillarySliceState, iSliceState } from "@/types";
 import { FiEdit2 } from "react-icons/fi";
 import { HiTrash } from "react-icons/hi2";
 import { FaPlus } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+import {
+  useGetAvailableActionsQuery,
+  useGetAvailableTriggersQuery,
+} from "@/store/api/ancillary";
 
 const Node = ({
   action,
@@ -20,18 +25,40 @@ const Node = ({
   handleDeleteActionNode?: (nodeId: number) => void;
 }) => {
   const title = action ? action?.title : trigger?.title;
-  const label = action ? action?.label : trigger?.label;
+  const [label, setLabel] = useState<string | undefined>();
+
+  const { isFetching: isActionsFetching, data: availableActions } =
+    useGetAvailableActionsQuery(null);
+  const { isFetching: isTriggerFetching, data: availableTrigger } =
+    useGetAvailableTriggersQuery(null);
+
+  useEffect(() => {
+    if (action) {
+      const name = availableActions?.data?.find(
+        (act) => act.id === action?.availableActionId,
+      )?.name;
+      setLabel(name ?? "Action");
+    }
+    if (trigger) {
+      const name = availableTrigger?.data?.find(
+        (trigg) => trigg.id === trigger?.availableTriggerId,
+      )?.name;
+      setLabel(name ?? "Trigger");
+    }
+  }, [action, trigger, availableActions, availableTrigger]);
+
   const icon = action ? action?.icon : trigger?.icon;
   const id = action ? action?.id : trigger?.id;
 
   if ((!action && !trigger) || (action && trigger)) return null;
+  if (isActionsFetching || isTriggerFetching) return <h1>Loading...</h1>;
   return (
     <>
       <div className="ring-2 rounded-lg px-4 py-2">
         <div className="flex justify-between">
           <div className="flex place-items-center gap-2 text-xs">
             {icon && React.createElement(icon, { className: "size-4" })}
-            <p>{title}</p>
+            <p>{label}</p>
           </div>
           <div className="flex place-items-center gap-1">
             <Button variant="ghost" size="icon" onClick={() => onEdit(id!)}>
@@ -51,7 +78,7 @@ const Node = ({
         <div>
           <p>
             <strong>{id! + 1}.&nbsp;&nbsp;</strong>
-            <span>{label}</span>
+            <span>{title}</span>
           </p>
         </div>
       </div>
