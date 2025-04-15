dd18f21b1c78b884790bdcfe3221d110d96bf16d
Manish Suthar <manishsuthar078@gmail.com>
2025-04-15 12:36:11 +0530

    impr: initial state added with type safety

--- apps/web/store/slices/ancillary.ts
+++ apps/web/store/slices/ancillary.ts
@@ -1,11 +1,32 @@
+import { iAncillarySliceState } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
+import { BsFillLightningChargeFill } from "react-icons/bs";
+import { LuPlugZap } from "react-icons/lu";
+
+const initialState: iAncillarySliceState = {
+  trigger: {
+    id: 0,
+    title: "Trigger",
+    label: "Add a custom trigger label",
+    icon: LuPlugZap,
+    availableTriggerId: "",
+    triggerMetadata: {},
+  },
+  actions: [
+    {
+      id: 1,
+      title: "Action",
+      label: "Add a custom action label",
+      icon: BsFillLightningChargeFill,
+      availableActionId: "",
+      actionMetadata: {},
+    },
+  ],
+};
 
 const ancillarySlice = createSlice({
   name: "ancillarySlice",
-  initialState: {
-    trigger: null,
-    actions: null,
-  },
+  initialState,
   reducers: {
     setTrigger: (state, action) => {
       state.trigger = action.payload;
