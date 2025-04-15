b29b68609fb739015bfed716144bb30080fafba5
Manish Suthar <manishsuthar078@gmail.com>
2025-04-15 12:44:51 +0530

    refactor(Node.tsx): added Node tail to component itself. taking handleAddActionNode and handleDeleteActionNode as props
    Removed handleDeleteActionNode in favor of handleDeleteActionNode in page.tsx
    Removed id and icon label in favor of 	rigger and ction these will have the icon and id fields embedded

--- apps/web/app/(protected)/zaps/publish/Node.tsx
+++ apps/web/app/(protected)/zaps/publish/Node.tsx
@@ -1,65 +1,75 @@
 "use client";
+import React from "react";
 import { Button } from "@/components/ui/button";
+import { iAncillarySliceState } from "@/types";
 import { FiEdit2 } from "react-icons/fi";
 import { HiTrash } from "react-icons/hi2";
+import { FaPlus } from "react-icons/fa6";
 
 const Node = ({
-  id,
-  icon,
   action,
   trigger,
   onEdit,
+  handleAddActionNode,
+  handleDeleteActionNode,
 }: {
+  action?: iAncillarySliceState["actions"][number];
+  trigger?: iAncillarySliceState["trigger"];
   onEdit: (nodeId: number) => void;
-  id: number;
-  icon: React.ReactNode;
-  action?: Record<string, string>;
-  trigger?: Record<string, string>;
+  handleAddActionNode: (nodeId: number) => void;
+  handleDeleteActionNode?: (nodeId: number) => void;
 }) => {
+  const title = action ? action?.title : trigger?.title;
+  const label = action ? action?.label : trigger?.label;
+  const icon = action ? action?.icon : trigger?.icon;
+  const id = action ? action?.id : trigger?.id;
+
   if ((!action && !trigger) || (action && trigger)) return null;
-
-  const title = action ? action.title : trigger?.title;
-  const label = action ? action.label : trigger?.label;
-
-  const handleDeleteAction = () => {
-    // Handle delete action
-    if (!action) return;
-    const confirmation = window.confirm(
-      "Are you sure you want to delete this action?",
-    );
-    if (!confirmation) return;
-    console.log("Delete action", id);
-  };
-
   return (
-    <div className="ring-2 rounded-lg px-4 py-2">
-      <div className="flex justify-between">
-        <div className="flex place-items-center gap-2 text-xs">
-          {icon}
-          <p>{title}</p>
-        </div>
-        <div className="flex place-items-center gap-1">
-          <Button variant="ghost" size="icon" onClick={() => onEdit(id)}>
-            <FiEdit2 className="cursor-pointer size-4" />
-          </Button>
-          {action && (
-            <Button
-              onClick={() => handleDeleteAction()}
-              variant={"ghost"}
-              size="icon"
-            >
-              <HiTrash className="cursor-pointer text-red-500 size-4" />
+    <>
+      <div className="ring-2 rounded-lg px-4 py-2">
+        <div className="flex justify-between">
+          <div className="flex place-items-center gap-2 text-xs">
+            {icon && React.createElement(icon, { className: "size-4" })}
+            <p>{title}</p>
+          </div>
+          <div className="flex place-items-center gap-1">
+            <Button variant="ghost" size="icon" onClick={() => onEdit(id!)}>
+              <FiEdit2 className="cursor-pointer size-4" />
             </Button>
-          )}
+            {action && (
+              <Button
+                onClick={() => handleDeleteActionNode?.(id!)}
+                variant={"ghost"}
+                size="icon"
+              >
+                <HiTrash className="cursor-pointer text-red-500 size-4" />
+              </Button>
+            )}
+          </div>
+        </div>
+        <div>
+          <p>
+            <strong>{id! + 1}.&nbsp;&nbsp;</strong>
+            <span>{label}</span>
+          </p>
         </div>
       </div>
+      {/* Node tail */}
       <div>
-        <p>
-          <strong>{id + 1}.&nbsp;&nbsp;</strong>
-          <span>{label}</span>
-        </p>
+        <span className="block mx-auto h-8 w-1 rounded-full bg-foreground" />
+        <div className="mx-auto w-fit">
+          <Button
+            size="icon"
+            onClick={() => handleAddActionNode(id!)}
+            className="my-1 rounded-full"
+          >
+            <FaPlus />
+          </Button>
+        </div>
+        <span className="block mx-auto h-8 w-1 rounded-full bg-foreground" />
       </div>
-    </div>
+    </>
   );
 };
 export default Node;
