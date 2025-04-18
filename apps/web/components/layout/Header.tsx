a71f9fa622ab1a09a8c00ecea4a8f900baa29f45
Manish Suthar <manishsuthar078@gmail.com>
2025-04-18 12:32:55 +0530

    impr(canvas): Default Header made invisible for canvas pages e.g., /zaps/pubish and /zaps/:zapId

--- apps/web/components/layout/Header.tsx
+++ apps/web/components/layout/Header.tsx
@@ -6,12 +6,14 @@ import Link from "next/link";
 import { IoPersonOutline } from "react-icons/io5";
 import { ToggleThemeMode } from "../ui/ToggleThemeMode";
 import { useMeQuery } from "@/store/api/user";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
   const { isLoading, data } = useMeQuery(null);
   const [scroll, setScroll] = useState(false);
+  const pathname = usePathname();
+  const isZapsCanvasRoute = pathname.startsWith("/zaps/");
 
-  console.log(data);
   useEffect(() => {
     const handleScroll = () => {
       setScroll(window.scrollY > 40);
@@ -22,7 +24,7 @@ const Header = () => {
 
   return (
     <div
-      className={`flex transition-all duration-300  fixed top-0 inset-x-0 z-50 justify-between items-center py-1 px-4 ${scroll && "bg-background/80 backdrop-blur-md"} `}
+      className={`flex transition-all duration-300 fixed top-0 inset-x-0 z-50 justify-between items-center py-1 px-4 ${scroll && "bg-background/80 backdrop-blur-md"} ${isZapsCanvasRoute && "hidden"}`}
     >
       <Link href={"/"} className="flex items-center gap-2">
         <div className="flex items-center justify-center gap-2">
@@ -63,6 +65,50 @@ const Header = () => {
         )}
 
         <ToggleThemeMode />
+      </div>
+    </div>
+  );
+};
+
+export const CanvasHeader = () => {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY > 20);
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const pathname = usePathname();
+  const publishZap = () => {};
+
+  return (
+    <div
+      className={`flex transition-all duration-300 fixed top-0 py-2 inset-x-0 z-50 justify-between items-center py-1 px-4 ${scroll && "bg-background/80 backdrop-blur-md"}`}
+    >
+      <Link href={"/"} className="flex items-center gap-2">
+        <div className="flex items-center justify-center gap-2">
+          <h1>
+            <span className="text-3xl font-bold">FlowLink</span>
+            <small className="text-xs">&nbsp;Canvas</small>
+          </h1>
+        </div>
+      </Link>
+      <div className="flex gap-4 px-8 max-md:px-4">
+        {pathname.startsWith("/zaps/publish") ? (
+          <button
+            onClick={publishZap}
+            className="bg-orange-500 rounded-full px-3 py-1"
+          >
+            Publish
+          </button>
+        ) : (
+          <button className="bg-orange-500 rounded-full px-3 py-1">
+            <Link href="/zaps/publish">New Zap </Link>
+          </button>
+        )}
       </div>
     </div>
   );
