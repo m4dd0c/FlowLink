6009805acb378da43d8fccb20126fb7c7aae3301
Manish Suthar <manishsuthar078@gmail.com>
2025-04-18 15:14:23 +0530

    feat: logout func used

--- apps/web/app/(protected)/me/page.tsx
+++ apps/web/app/(protected)/me/page.tsx
@@ -1,18 +1,36 @@
 "use client";
 import React, { useEffect } from "react";
-import { useMeQuery } from "@/store/api/user";
+import { useLazyLogoutQuery, useMeQuery } from "@/store/api/user";
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
   const { data, isFetching, isError } = useMeQuery(null);
 
+  const [name, setName] = React.useState<string | null>(null);
   useEffect(() => {
     if (isError) {
       console.error("Error fetching user data");
     }
   }, [isError]);
 
+  useEffect(() => {
+    if (data?.data) {
+      setName(data.data.name);
+    }
+  }, [data?.data]);
+  const [trigger] = useLazyLogoutQuery();
+  const logout = () => {
+    trigger(null);
+  };
+
   if (isFetching) return <div>Loading...</div>;
-  return <div>this is profile page {JSON.stringify(data?.data)}</div>;
+  return (
+    <div>
+      {name}
+      <br />
+      {data?.data && <Button onClick={logout}>Logout</Button>}
+    </div>
+  );
 };
 
 export default Page;
