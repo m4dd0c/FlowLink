3f8044ce69f63933661f3529b3323a36d26b7a98
Manish Suthar <manishsuthar078@gmail.com>
2025-04-18 16:32:15 +0530

    mics(nodes & dev): Added ViewDetails Button and function without implementation.
    Toggling between Edit and ViewDetails Buttons based on the path.
    If pathname is /zaps/publish than showing the EditNode button, Otherwise ViewDetails
    
    Deleted Action Button is Hidden in path other than /zaps/publish
    Add Action Button is Hidden in path other than /zaps/publish
    
    Plenty of comments added for understanding the code later

--- apps/web/components/Canvas/Node.tsx
+++ apps/web/components/Canvas/Node.tsx
@@ -4,11 +4,13 @@ import { Button } from "@/components/ui/button";
 import { iAncillarySliceState } from "@/types";
 import { FiEdit2 } from "react-icons/fi";
 import { HiTrash } from "react-icons/hi2";
-import { FaPlus } from "react-icons/fa6";
+import { FaPlus, FaRegEye } from "react-icons/fa6";
 import {
   useGetAvailableActionsQuery,
   useGetAvailableTriggersQuery,
 } from "@/store/api/ancillary";
+import { LuPlugZap } from "react-icons/lu";
+import { usePathname } from "next/navigation";
 
 const Node = ({
   action,
@@ -23,21 +25,32 @@ const Node = ({
   handleAddActionNode?: (nodeId: number) => void;
   handleDeleteActionNode?: (nodeId: number) => void;
 }) => {
-  const title = action ? action?.title : trigger?.title;
-  const [label, setLabel] = useState<string | undefined>();
+  const pathname = usePathname();
+  const isPublishCanvasPath = pathname.startsWith("/zaps/publish");
 
+  const title = action ? action?.title : trigger?.title;
+
+  // Importing available actions and triggers for Label configuration e.g., "Webhook", "Gmail", etc.
   const { isFetching: isActionsFetching, data: availableActions } =
     useGetAvailableActionsQuery(null);
   const { isFetching: isTriggerFetching, data: availableTrigger } =
     useGetAvailableTriggersQuery(null);
 
+  // Configure the label for the node e.g., "Webhook", "Gmail", etc.
+  const [label, setLabel] = useState<string | undefined>();
   useEffect(() => {
+    // If the Node is Action Node.
+    // Then get the name from availableActions with corresponding id
+    // Default to "Action" if not found
     if (action) {
       const name = availableActions?.data?.find(
         (act) => act.id === action?.availableActionId,
       )?.name;
       setLabel(name ?? "Action");
     }
+    // If the Node is Trigger Node.
+    // Then get the name from availableActions with corresponding id
+    // Default to "Trigger" if not found
     if (trigger) {
       const name = availableTrigger?.data?.find(
         (trigg) => trigg.id === trigger?.availableTriggerId,
@@ -46,11 +59,20 @@ const Node = ({
     }
   }, [action, trigger, availableActions, availableTrigger]);
 
-  const icon = action ? action?.icon : trigger?.icon;
+  // TODO:Icon util function
+  // Icon configuration for the node
+  const icon = (action ? action?.icon : trigger?.icon) ?? LuPlugZap;
+
+  // Getting Id for the node for Delete and Edit operations
+  // Only useful, If the path is "/zaps/publish"
+  // If the path is "/zaps/publish" then it will be `number` else it will be `cuid`
   const id = action ? action?.id : trigger?.id;
 
   if ((!action && !trigger) || (action && trigger)) return null;
+
   if (isActionsFetching || isTriggerFetching) return <h1>Loading...</h1>;
+
+  const viewDetails = () => {};
   return (
     <>
       <div className="ring-2 rounded-lg px-4 py-2">
@@ -60,13 +82,30 @@ const Node = ({
             <p className="uppercase">{label}</p>
           </div>
           <div className="flex place-items-center gap-1">
-            <Button variant="ghost" size="icon" onClick={() => onEdit?.(id!)}>
-              <FiEdit2 className="cursor-pointer size-4" />
-            </Button>
-            {action && (
+            {isPublishCanvasPath && onEdit ? (
               <Button
-                onClick={() => handleDeleteActionNode?.(id!)}
+                title="Edit Node"
+                variant="ghost"
+                size="icon"
+                onClick={() => onEdit(id!)}
+              >
+                <FiEdit2 className="cursor-pointer size-4" />
+              </Button>
+            ) : (
+              <Button
+                variant="ghost"
+                title="View Details"
+                size="icon"
+                onClick={() => viewDetails()}
+              >
+                <FaRegEye className="cursor-pointer size-4" />
+              </Button>
+            )}
+            {isPublishCanvasPath && handleDeleteActionNode && action && (
+              <Button
+                onClick={() => handleDeleteActionNode(id!)}
                 variant={"ghost"}
+                title="Delete Node"
                 size="icon"
               >
                 <HiTrash className="cursor-pointer text-red-500 size-4" />
@@ -83,11 +122,11 @@ const Node = ({
       {/* Node tail */}
       <div>
         <span className="block mx-auto h-8 w-1 bg-foreground" />
-        {handleAddActionNode && (
+        {isPublishCanvasPath && handleAddActionNode && (
           <div className="mx-auto w-fit">
             <Button
               size="icon"
-              onClick={() => handleAddActionNode?.(id!)}
+              onClick={() => handleAddActionNode(id!)}
               className="my-1 rounded-full"
             >
               <FaPlus />
