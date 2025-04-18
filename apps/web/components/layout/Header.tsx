1b2b72e4fa3e23874ccadfc3b5103deebc12078a
Manish Suthar <manishsuthar078@gmail.com>
2025-04-18 14:39:44 +0530

    refactor: redundent Canvas header removed since we had it already in CanvasHeader.tsx

--- apps/web/components/layout/Header.tsx
+++ apps/web/components/layout/Header.tsx
@@ -70,48 +70,4 @@ const Header = () => {
   );
 };
 
-export const CanvasHeader = () => {
-  const [scroll, setScroll] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScroll(window.scrollY > 20);
-    };
-    document.addEventListener("scroll", handleScroll);
-    return () => document.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const pathname = usePathname();
-  const publishZap = () => {};
-
-  return (
-    <div
-      className={`flex transition-all duration-300 fixed top-0 py-2 inset-x-0 z-50 justify-between items-center py-1 px-4 ${scroll && "bg-background/80 backdrop-blur-md"}`}
-    >
-      <Link href={"/"} className="flex items-center gap-2">
-        <div className="flex items-center justify-center gap-2">
-          <h1>
-            <span className="text-3xl font-bold">FlowLink</span>
-            <small className="text-xs">&nbsp;Canvas</small>
-          </h1>
-        </div>
-      </Link>
-      <div className="flex gap-4 px-8 max-md:px-4">
-        {pathname.startsWith("/zaps/publish") ? (
-          <button
-            onClick={publishZap}
-            className="bg-orange-500 rounded-full px-3 py-1"
-          >
-            Publish
-          </button>
-        ) : (
-          <button className="bg-orange-500 rounded-full px-3 py-1">
-            <Link href="/zaps/publish">New Zap </Link>
-          </button>
-        )}
-      </div>
-    </div>
-  );
-};
-
 export default Header;
