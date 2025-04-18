7d8c54a680b7e7f6c0e1a9d2d184b1388b00ea4c
Manish Suthar <manishsuthar078@gmail.com>
2025-04-18 12:31:16 +0530

    refactor: zap title removed and now each action and each trigger will have their own titles
    Trigger Title will be used as ZapTitle

--- apps/backend/src/lib/zodSchemas/schema.ts
+++ apps/backend/src/lib/zodSchemas/schema.ts
@@ -16,14 +16,18 @@ const ZapIdSchema = z.object({
 });
 
 const ZapCreateSchema = z.object({
+  availableTriggerId: z.string(),
+  triggerMetadata: z.any().optional(),
   title: z
     .string()
     .min(3, "Title must contain at least 3 characters.")
     .max(40, "Title must contain at most 40 characters."),
-  availableTriggerId: z.string(),
-  triggerMetadata: z.any().optional(),
   actions: z.array(
     z.object({
+      title: z
+        .string()
+        .min(3, "Title must contain at least 3 characters.")
+        .max(40, "Title must contain at most 40 characters."),
       availableActionId: z.string(),
       actionMetadata: z.any().optional(),
     }),
