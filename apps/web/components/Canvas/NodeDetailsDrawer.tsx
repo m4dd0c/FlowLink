4e21a8e4fe808a2cd764d47a784f56ad1a1dcc44
Manish Suthar <manishsuthar078@gmail.com>
2025-04-19 22:59:28 +0530

    refactor: NodeDetailsCard added and used as it is moved to separate file.,
    Removed DetailsCard in favor of NodeDetailsCard

--- apps/web/components/Canvas/NodeDetailsDrawer.tsx
+++ apps/web/components/Canvas/NodeDetailsDrawer.tsx
@@ -10,36 +10,7 @@ import {
   DrawerTitle,
 } from "../ui/drawer";
 import { Button } from "../ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
-import { Label } from "../ui/label";
-import { Input } from "../ui/Input";
-import { WEBHOOK_BE_URL } from "@/lib/constants";
-
-const DetailsCard = ({ node }: { node: any }) => {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{node.label}</CardTitle>
-        <CardDescription>{node.title}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <h1 className="font-bold">Metadata</h1>
-        <p className="text-muted-foreground">{node.metadata}</p>
-        <Label htmlFor="url"></Label>
-        <Input
-          name="url"
-          value={`${WEBHOOK_BE_URL}/hooks/catch/${node?.userId}/${node?.zapId}`}
-        />
-      </CardContent>
-    </Card>
-  );
-};
+import NodeDetailsCard from "./NodeDetailsCard";
 
 const NodeDetailsDrawer = ({
   open,
@@ -60,7 +31,7 @@ const NodeDetailsDrawer = ({
               See details about your {node?.type}
             </DrawerDescription>
           </DrawerHeader>
-          <DetailsCard node={node} />
+          <NodeDetailsCard node={node} />
           <DrawerFooter>
             <DrawerClose asChild>
               <Button variant="outline">Cancel</Button>
